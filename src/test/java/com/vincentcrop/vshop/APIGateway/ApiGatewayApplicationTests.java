053cd78d9cdabb2fc361efec0aa5bca1620e56e3
VincentB00 <vincentbui1999@gmail.com>
2023-04-04 19:40:50 -0400

    remove test

--- src/test/java/com/vincentcrop/vshop/APIGateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/vincentcrop/vshop/APIGateway/ApiGatewayApplicationTests.java
@@ -1,9 +1,5 @@
 package com.vincentcrop.vshop.APIGateway;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.regex.Pattern;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,14 +8,6 @@ class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
-
-	@Test
-	void Test()
-	{
-		String path = "/user/1/ok";
-
-		assertTrue(Pattern.matches("/user/\\d/ok", path));
-	}
-
 }
