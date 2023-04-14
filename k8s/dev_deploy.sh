464af9a9373cbe6c0763c7674281b45af33957c2
VincentB00 <vincentbui1999@gmail.com>
2023-04-14 19:01:38 -0400

    fix dev_deploy.sh

--- k8s/dev_deploy.sh
+++ k8s/dev_deploy.sh
@@ -1,5 +1,4 @@
 namespace=api-gateway-dev
-cd k8s
 microk8s kubectl delete namespace ${namespace}
 microk8s kubectl create namespace ${namespace}
 microk8s kubectl -n ${namespace} apply -f ${namespace}_deployment.yaml
