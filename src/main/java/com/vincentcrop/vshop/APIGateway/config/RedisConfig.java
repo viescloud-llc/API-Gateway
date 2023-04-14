470d1adfe8c63663d85fa46c70e42e1a2976eafa
VincentB00 <vincentbui1999@gmail.com>
2023-04-14 17:13:34 -0400

    update vault key

--- src/main/java/com/vincentcrop/vshop/APIGateway/config/RedisConfig.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/config/RedisConfig.java
@@ -23,7 +23,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class RedisConfig extends CachingConfigurerSupport implements CachingConfigurer
 {
-
     @Value("${spring.data.redis.host}")
     private String redisHost;
 
