9f9462f277d48bb7a8afd7f30da39ebb104e3905
VincentB00 <vincentbui1999@gmail.com>
2023-04-16 13:32:16 -0400

    fix K8s and health check

--- src/main/java/com/vincentcrop/vshop/APIGateway/ApiGatewayApplication.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/ApiGatewayApplication.java
@@ -4,14 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@RestController
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@GetMapping("/_status/healthz")
+	public String healthCheck() {
+		return "API Gateway up and running";
 	}
 }
