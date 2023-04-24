2cc3561f159b3995092a6ee648e7896fef1cee66
VincentB00 <vincentbui1999@gmail.com>
2023-04-24 17:25:13 -0400

    add cors config

--- src/main/java/com/vincentcrop/vshop/APIGateway/config/BeanConfig.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/config/BeanConfig.java
@@ -3,6 +3,8 @@ package com.vincentcrop.vshop.APIGateway.config;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.ObjectProvider;
@@ -13,6 +15,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,10 +29,19 @@ import com.google.gson.Gson;
 public class BeanConfig 
 {
     @Bean
-    public HttpMessageConverters messageConverter(ObjectProvider<HttpMessageConverter<?>> converter)
-    {
-        return new HttpMessageConverters(converter.orderedStream().collect(Collectors.toList()));
-    }   
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("*"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }  
 
     @Bean
     public WebFluxConfigurer corsConfigurer() {
@@ -55,6 +69,12 @@ public class BeanConfig
             }
         };
     }
+
+    @Bean
+    public HttpMessageConverters messageConverter(ObjectProvider<HttpMessageConverter<?>> converter)
+    {
+        return new HttpMessageConverters(converter.orderedStream().collect(Collectors.toList()));
+    }   
 
     // @Bean
     // public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
--- src/main/java/com/vincentcrop/vshop/APIGateway/config/GatewayConfig.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/config/GatewayConfig.java
@@ -2,6 +2,7 @@ package com.vincentcrop.vshop.APIGateway.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,5 +32,13 @@ public class GatewayConfig
 				.filters(f -> f.filter(authenticationFilter).stripPrefix(1))
                 .uri("lb://ITEM-SERVICE"))
             .build();
+    }
+
+    private GatewayFilterSpec addCors(GatewayFilterSpec gatewayFilterSpec)
+    {
+        gatewayFilterSpec.addResponseHeader("Access-Control-Allow-Origin", "*");
+        gatewayFilterSpec.addResponseHeader("Access-Control-Allow-Methods", "*");
+        gatewayFilterSpec.addResponseHeader("Access-Control-Allow-Headers", "*");
+        return gatewayFilterSpec;
     }
 }
