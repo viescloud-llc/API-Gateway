5a995e09946506c80d5fd31c9b622ecd3fad25fe
VincentB00 <vincentbui1999@gmail.com>
2023-05-01 15:33:41 -0400

    fix deployment IP

--- src/main/java/com/vincentcrop/vshop/APIGateway/ApiGatewayApplication.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/ApiGatewayApplication.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiGatewayApplication {
 
 	@Value("${spring.profiles.active}")
-	private String env = "dev";
+	private String env = "?";
 
 	public static void main(String[] args) 
 	{
