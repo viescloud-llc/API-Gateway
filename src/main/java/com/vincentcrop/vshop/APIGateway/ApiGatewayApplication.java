522b93d524b7b0093a670accc70b7064f1e95580
VincentB00 <vincentbui1999@gmail.com>
2023-05-15 20:01:32 -0400

    move default controller

--- src/main/java/com/vincentcrop/vshop/APIGateway/ApiGatewayApplication.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/ApiGatewayApplication.java
@@ -1,29 +1,17 @@
 package com.vincentcrop.vshop.APIGateway;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@RestController
 public class ApiGatewayApplication {
-
-	@Value("${spring.profiles.active}")
-	private String env = "?";
 
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
-	@GetMapping("/_status/healthz")
-	public String healthCheck() {
-		return String.format("API Gateway %s is up and running", env);
 	}
 }
--- src/main/java/com/vincentcrop/vshop/APIGateway/controller/DefaultController.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/controller/DefaultController.java
@@ -1,7 +1,9 @@
 package com.vincentcrop.vshop.APIGateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,14 @@ public class DefaultController {
 
     @Autowired
     private AuthenticatorClient authenticatorClient;
+
+    @Value("${spring.profiles.active}")
+	private String env = "?";
+
+    @GetMapping("/_status/healthz")
+	public String healthCheck() {
+		return String.format("API Gateway %s is up and running", env);
+	}
 
     @PostMapping("/login")
     public ResponseEntity<Jwt> login(@RequestBody User user) {
