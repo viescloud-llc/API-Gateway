c9cae886c59068b3455f3b43cabeeb19b557bcd8
VincentB00 <vincentbui1999@gmail.com>
2023-06-02 15:29:15 -0400

    add throw response status exception

--- src/main/java/com/vincentcrop/vshop/APIGateway/config/filter/AuthenticationFilter.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/config/filter/AuthenticationFilter.java
@@ -14,6 +14,7 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 
 import com.vincentcrop.vshop.APIGateway.model.authenticator.Role;
@@ -27,19 +28,16 @@ import reactor.core.publisher.Mono;
 
 @RefreshScope
 @Component
-public class AuthenticationFilter implements GatewayFilter
-{
+public class AuthenticationFilter implements GatewayFilter {
     private final String DEFAULT_ROLE_OWNER = "OWNER";
     private final String DEFAULT_ROLE_CO_OWNER = "CO-OWNER";
- 
+
     @Autowired
     private AuthenticatorService authenticatorService;
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) 
-    {
-        try
-        {
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        try {
             ServerHttpRequest request = exchange.getRequest();
 
             String requestMethod = request.getMethod().name();
@@ -47,20 +45,20 @@ public class AuthenticationFilter implements GatewayFilter
 
             final String token = this.getBearerJwt(request);
             User user = this.authenticatorService.getUser(token);
-                
+
             if (!isValidRoute(path, requestMethod, user)) {
                 if (this.isAuthMissing(request))
-                    return this.onError(exchange, "Authorization header is missing in request", HttpStatus.UNAUTHORIZED);
+                    return this.onError(exchange, "Authorization header is missing in request",
+                            HttpStatus.UNAUTHORIZED);
 
                 return this.onError(exchange, "Unauthorized", HttpStatus.UNAUTHORIZED);
             }
-            
-            if(!ObjectUtils.isEmpty(user))
+
+            if (!ObjectUtils.isEmpty(user))
                 this.populateRequestWithHeaders(exchange, user);
-        }
-        catch(Exception ex)
-        {
-            ex.printStackTrace();
+        } catch (ResponseStatusException ex) {
+            throw ex;
+        } catch (Exception ex) {
             Splunk.logError(ex);
             HttpResponseThrowers.throwServerError("server is experiencing some error");
         }
@@ -69,7 +67,8 @@ public class AuthenticationFilter implements GatewayFilter
     }
 
     private int routeCompare(Route a, Route b) {
-        // int splitLength = Integer.compare(b.getPath().split("/").length, a.getPath().split("/").length);
+        // int splitLength = Integer.compare(b.getPath().split("/").length,
+        // a.getPath().split("/").length);
         int charLength = Integer.compare(b.getPath().length(), a.getPath().length());
 
         // return splitLength > charLength ? splitLength : charLength;
@@ -81,108 +80,108 @@ public class AuthenticationFilter implements GatewayFilter
      * 1. exact path get check first
      * 2. long path get match first
      * 3. wild card path get match last
-     * @param path to be match
+     * 
+     * @param path          to be match
      * @param requestMethod to be match
-     * @param user login user
+     * @param user          login user
      * @return true if a path match is found else false
      */
-    public boolean isValidRoute(String path, String requestMethod, User user)
-    {
-        //return true if this is super user
-        if(isSuperPowerUser(user))
+    public boolean isValidRoute(String path, String requestMethod, User user) {
+        // return true if this is super user
+        if (isSuperPowerUser(user))
             return true;
 
-        //sort the list in length desc
+        // sort the list in length desc
         List<Route> routes = this.authenticatorService.getAllRoute();
         routes = routes.parallelStream().sorted((a, b) -> routeCompare(a, b)).collect(Collectors.toList());
 
-        //exact path match check
+        // exact path match check
         AtomicBoolean matchCriteria = new AtomicBoolean(false);
         boolean anyMatchExact = routes.parallelStream().anyMatch(e -> {
             String ePath = e.getPath();
             boolean anyMatchExactI = requestMethod.equals(e.getMethod()) && ePath.equals(path);
 
-            if(anyMatchExactI && !e.isSecure())
+            if (anyMatchExactI && !e.isSecure())
                 matchCriteria.set(true);
-            else if(anyMatchExactI && !ObjectUtils.isEmpty(user))
+            else if (anyMatchExactI && !ObjectUtils.isEmpty(user))
                 matchCriteria.set(!e.isSecure() || anyMatchRole(e.getRoles(), user.getUserRoles()));
 
             return anyMatchExactI;
         });
 
-        if(anyMatchExact)
+        if (anyMatchExact)
             return matchCriteria.get();
-        
-        //match path check
+
+        // match path check
         matchCriteria.set(false);
         boolean anyMatch = routes.stream().anyMatch(e -> {
             String ePath = e.getPath();
             Boolean match = requestMethod.equals(e.getMethod()) && Pattern.matches("^" + ePath, path);
 
-            if(match && !e.isSecure())
+            if (match && !e.isSecure())
                 matchCriteria.set(true);
-            else if(match && !ObjectUtils.isEmpty(user))
+            else if (match && !ObjectUtils.isEmpty(user))
                 matchCriteria.set(anyMatchRole(e.getRoles(), user.getUserRoles()));
 
             return match;
         });
 
-        if(anyMatch)
+        if (anyMatch)
             return matchCriteria.get();
 
         // if(ObjectUtils.isEmpty(user) || !user.isEnable())
-        //     return false;
-        
+        // return false;
+
         return false;
     }
 
     public boolean isSuperPowerUser(User user) {
-        return !ObjectUtils.isEmpty(user) && user.getUserRoles().parallelStream().anyMatch(r -> r.getName().equals(DEFAULT_ROLE_OWNER) || r.getName().equals(DEFAULT_ROLE_CO_OWNER));
+        return !ObjectUtils.isEmpty(user) && user.getUserRoles().parallelStream()
+                .anyMatch(r -> r.getName().equals(DEFAULT_ROLE_OWNER) || r.getName().equals(DEFAULT_ROLE_CO_OWNER));
     }
 
-    public boolean anyMatchRole(List<Role> originRole, List<Role> targetRole)
-    {
-        if(originRole == null || targetRole == null)
+    public boolean anyMatchRole(List<Role> originRole, List<Role> targetRole) {
+        if (originRole == null || targetRole == null)
             return false;
-            
+
         return originRole.parallelStream().anyMatch(or -> targetRole.parallelStream().anyMatch(tr -> tr.equals(or)));
     }
 
     /**
      * check if path is a secure path
+     * 
      * @deprecated
-     * this method is no longer use due to inaccurate checking
-     * <p> use {@link #isValidRoute(String, String, User) isValidRoute} instead
-     * @param path to be match
+     *             this method is no longer use due to inaccurate checking
+     *             <p>
+     *             use {@link #isValidRoute(String, String, User) isValidRoute}
+     *             instead
+     * @param path          to be match
      * @param requestMethod to be match
-     * @param routes to be match with
+     * @param routes        to be match with
      * @return true if a path match is found else false
      */
     @Deprecated
-    public boolean isSecure(String path, String requestMethod, List<Route> routes)
-    {
+    public boolean isSecure(String path, String requestMethod, List<Route> routes) {
         return routes.parallelStream().noneMatch(e -> {
             String ePath = e.getPath();
             return e.isSecure() && requestMethod.equals(e.getMethod()) && Pattern.matches("^" + ePath, path);
         });
     }
 
-    public Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) 
-    {
+    public Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(httpStatus);
         return response.setComplete();
     }
 
-    public String getBearerJwt(ServerHttpRequest request) 
-    {
+    public String getBearerJwt(ServerHttpRequest request) {
         List<String> headers = request.getHeaders().getOrEmpty("Authorization");
-        if(headers.isEmpty())
+        if (headers.isEmpty())
             return null;
 
         String bearer = headers.get(0);
 
-        if(!(bearer.contains("Bearer") || bearer.contains("bearer")))
+        if (!(bearer.contains("Bearer") || bearer.contains("bearer")))
             HttpResponseThrowers.throwBadRequest("Token is not bearer token");
 
         return bearer;
@@ -191,19 +190,17 @@ public class AuthenticationFilter implements GatewayFilter
     public static String extractJwt(String Authorization) {
         String[] tokens = Authorization.split(" ");
         String token = null;
-        if(tokens.length > 1)
+        if (tokens.length > 1)
             token = tokens[tokens.length - 1];
 
         return token;
     }
 
-    public boolean isAuthMissing(ServerHttpRequest request) 
-    {
+    public boolean isAuthMissing(ServerHttpRequest request) {
         return !request.getHeaders().containsKey("Authorization");
     }
 
-    public void populateRequestWithHeaders(ServerWebExchange exchange, User user) 
-    {
+    public void populateRequestWithHeaders(ServerWebExchange exchange, User user) {
         exchange.getRequest().mutate()
                 .header("user_id", user.getId() + "")
                 .build();
