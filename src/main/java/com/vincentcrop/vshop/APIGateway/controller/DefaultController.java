6ea449724b2936bae34a020546c17e01f6ca84d9
VincentB00 <vincentbui1999@gmail.com>
2023-06-02 17:01:35 -0400

    remove un-use import

--- src/main/java/com/vincentcrop/vshop/APIGateway/controller/DefaultController.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/controller/DefaultController.java
@@ -2,7 +2,6 @@ package com.vincentcrop.vshop.APIGateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
