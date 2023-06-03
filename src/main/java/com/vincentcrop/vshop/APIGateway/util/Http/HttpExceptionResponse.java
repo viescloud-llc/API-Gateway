9004e314dbf696f80d0b9ec66a3ba4dca1a4d3e9
VincentB00 <vincentbui1999@gmail.com>
2023-06-03 14:07:08 -0400

    fix http exception response

--- src/main/java/com/vincentcrop/vshop/APIGateway/util/Http/HttpExceptionResponse.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/util/Http/HttpExceptionResponse.java
@@ -12,10 +12,14 @@ import lombok.NoArgsConstructor;
 public class HttpExceptionResponse {
     private HttpStatus status;
     private String message;
+    private String reason;
+    private String localizedMessage;
 
     public HttpExceptionResponse(ResponseStatusException ex) {
         this. message = ex.getMessage();
         this.status = new HttpStatus(ex.getStatusCode());
+        this.reason = ex.getReason();
+        this.localizedMessage = ex.getLocalizedMessage();
     }
 
 }
