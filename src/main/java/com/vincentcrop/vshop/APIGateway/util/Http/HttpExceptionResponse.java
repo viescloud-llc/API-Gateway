37c6518db3ebd70c0a31765a7604e5e71792e6fb
VincentB00 <vincentbui1999@gmail.com>
2023-06-03 22:53:08 -0400

    add mask for http exception response

--- src/main/java/com/vincentcrop/vshop/APIGateway/util/Http/HttpExceptionResponse.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/util/Http/HttpExceptionResponse.java
@@ -2,6 +2,8 @@ package com.vincentcrop.vshop.APIGateway.util.Http;
 
 import org.springframework.web.server.ResponseStatusException;
 
+import com.vincentcrop.vshop.APIGateway.util.Time;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HttpExceptionResponse {
+    private String UUID;
+    private String time;
     private HttpStatus status;
     private String message;
+    private String detailMessageCode;
     private String reason;
     private String localizedMessage;
 
@@ -19,10 +24,13 @@ public class HttpExceptionResponse {
     private final static String CUT_MESSAGE2 = "\\\"";
 
     public HttpExceptionResponse(ResponseStatusException ex) {
+        this.UUID = java.util.UUID.randomUUID().toString();
+        this.time = Time.now().toSpring();
         this. message = extractMessage(ex.getMessage());
         this.status = new HttpStatus(ex.getStatusCode());
         this.reason = ex.getReason();
         this.localizedMessage = ex.getLocalizedMessage();
+        this.detailMessageCode = ex.getDetailMessageCode();
     }
 
     public static String extractMessage(String originMessage) {
@@ -50,5 +58,11 @@ public class HttpExceptionResponse {
         newMessage = newMessage.substring(0, index);
 
         return newMessage;
+    }
+
+    public void mask() {
+        this.reason = null;
+        this.localizedMessage = null;
+        this.detailMessageCode = null;
     }
 }
