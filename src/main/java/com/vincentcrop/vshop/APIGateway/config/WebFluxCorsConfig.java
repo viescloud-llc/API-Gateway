859302fc7389795397848e690118fe89521e7eb4
VincentB00 <vincentbui1999@gmail.com>
2024-06-03 14:25:45 -0400

    update webflux cors

--- src/main/java/com/vincentcrop/vshop/APIGateway/config/WebFluxCorsConfig.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/config/WebFluxCorsConfig.java
@@ -1,5 +1,8 @@
 package com.vincentcrop.vshop.APIGateway.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
@@ -12,9 +15,10 @@ public class WebFluxCorsConfig {
     public CorsWebFilter corsWebFilter() {
         org.springframework.web.cors.CorsConfiguration corsConfig = new org.springframework.web.cors.CorsConfiguration();
         corsConfig.setAllowCredentials(true);
-        corsConfig.addAllowedOrigin("*");
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.addAllowedHeader("*");
-        corsConfig.addAllowedMethod("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
         source.registerCorsConfiguration("/**", corsConfig);
