6d59cb0d935ce68b28c1bca25d2d04e7c30dfd8c
VincentB00 <vincentbui1999@gmail.com>
2024-08-22 19:42:24 -0400

    fix h2 in memory flag

--- src/main/java/com/vincentcrop/vshop/APIGateway/ApiGatewayApplication.java
+++ src/main/java/com/vincentcrop/vshop/APIGateway/ApiGatewayApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import com.vincent.inc.viesspringutils.config.BeanConfig;
+
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+		System.setProperty(BeanConfig.H2_IN_MEMORY_PROPERTY_PATH, "true");
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 }
